e45eaadf5cbf4b078fa6032fc4ed13cc628da358
Sebastian Coates <sebass@Sebastians-iMac.local>
2018-07-15 19:12:55 -0400

    tests better organized (folder and script options)

--- compiler/python/run_tests.sh
+++ compiler/python/run_tests.sh
@@ -2,6 +2,27 @@ touch output.txt
 rm output.txt
 touch output.txt
 
+test_file() 
+{
+        file=$1
+        name=${file#$2}
+        name=$(echo $name | cut -f 1 -d '.') #remove extension
+        python3 digon.py $file > tests/results/$name
+        touch output.txt
+        diff tests/expected/$name tests/results/$name >> output.txt
+        diff tests/expected/$name tests/results/$name > temp
+        [ -s temp ] || echo "$name passes test"
+        rm temp
+}
+
+test_files_in_dir()
+{
+        for file in tests/$1/*; do
+                test_file $file "tests/$1/"
+                echo $file
+        done   
+}
+
 if [ "$1" == "" ]; then
         echo "Please specify which tests to run"
         echo "To see list of available tests, run with --tests"
@@ -11,23 +32,26 @@ elif [ "$1" == "--help" -o "$1" == "-help" -o "$1" == "help" -o \
         echo "--tests: list available testing options"
         echo "Nothing else for now!"
 
-elif [ "$1" == "--tests" || ]; then #list test options
+elif [ "$1" == "--tests" ]; then #list test options
         echo "all: - run all tests available"
         echo "lexer: - run lexer tests (not yet ready)"
         echo "parser: - run parser tests (not yet ready)"
         echo "concurrent: - run tests for concurrency (not yet ready)"
 
 elif [ "$1" == "all" ]; then #run full tests
-        for file in tests/code/*; do
-                name=${file#tests/code/}
-                name=$(echo $name | cut -f 1 -d '.') #remove extension
-                python3 digon.py $file > tests/results/$name
-                touch output.txt
-                diff tests/expected/$name tests/results/$name >> output.txt
-                diff tests/expected/$name tests/results/$name > temp
-                [ -s temp ] || echo "$name passes test"
-                rm temp
-        done
+        test_files_in_dir "lexer"
+        test_files_in_dir "parser"
+        test_files_in_dir "concurrent"
+        test_files_in_dir "full"
+
+elif [ "$1" == "lexer" ]; then #run full tests
+        test_files_in_dir "lexer"
+elif [ "$1" == "parser" ]; then #run full tests
+        test_files_in_dir "parser"
+elif [ "$1" == "concurrent" ]; then #run full tests
+        test_files_in_dir "concurrent"
+elif [ "$1" == "full" ]; then #run full tests
+        test_files_in_dir "full"
 
 else #run specified tests by filename
         for file in "$@"; do
---
+++ compiler/python/tests/full/placeholder.di
@@ -0,0 +1,70 @@
+node root {
+        array := [10]int{1,2,3,4,5,6,7,8,9,1};
+        array => calculate_stats(data);
+}
+
+node calculate_stats <= (data [10]int){
+        data => mean() => print_data(average);
+        data => median() => print_data(median);
+        data => mode() => print_data(mode);
+}
+
+node mean <= (data1 [10]int) {
+        average := 0.0;
+        for _, elem in data1 {
+                average += float64(elem);
+        }
+
+        length := 0;
+        data1 => length() => length;
+        average / float64(length) => dest();
+}
+
+node median <= (data2 [10]int) {
+        length := 0;
+        data2 => length() => length;
+
+        for i, _ in data2 {
+                for j := i; j > 1; j-- {
+                        if data2[j] < data2[j - 1] {
+                                temp := data2[j - 1];
+                                data2[j - 1] = data2[j];
+                                data2[j] = temp;
+                        } else {
+                                break;
+                        }
+                }
+        }
+
+        data2[length / 2] => dest();
+}
+
+node mode <= (data3 [10]int) {
+        m := map<int, int>;
+
+        for _, elem in data3 {
+                _, valid := m[elem];
+                if !valid {
+                        m[elem] = 1;
+                } else {
+                        m[elem] += 1;
+                }
+        }
+
+        highest := 0;
+        mode := 0;
+        for key, value in m {
+                if value > highest {
+                        mode = key;
+                        highest = value;
+                }
+        }
+
+        mode => dest();
+}
+
+node print_data <= (average1 float, median1 int, mode1 int){
+        average1 => println();
+        median1 => println();
+        mode1 => println(); 
+}
